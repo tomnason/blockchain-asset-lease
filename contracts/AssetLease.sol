84b12201d28409eb95a2b03d792ae57a19c23755
Tom Nason <tom@nason.tech>
2021-11-18 17:20:16 +1100

    Contract prep

---
+++ contracts/AssetLease.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.4.25 <0.7.0;
+
+//import "./Ownable.sol";
+
+contract AssetLease {
+    
+/* 
+
+===========
+PLACEHOLDER
+===========
+
+OFFER
+
+ACCEPT
+
+EXTEND
+
+TRIGGER NOTICE
+
+TERMINATE
+
+CLAIM COLLATERAL
+
+DISPERSE COLLATERAL
+
+TRIGGER BALANCE
+
+WITHDRAW 
+
+*/
+
+}
